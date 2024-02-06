0569e51b16935a385b3a6a280b4fd7317bf5ce20
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-06 12:36:01 +0100

    add image

--- .utils/pytest.sh
+++ .utils/pytest.sh
@@ -2,4 +2,7 @@
 
 
 # .venv/bin/python3 -m pytest .
-python3 -m pytest .
+rm -f .assets/cov.svg
+rm -f .coverage
+coverage run -m pytest -vvx --capture=tee-sys --log-cli-level=INFO tests/
+coverage-badge -fo .assets/cov.svg
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+![image1](.assets/space1.png)
+
 # kaggle-spaceship-titanic
 https://www.kaggle.com/competitions/spaceship-titanic
